eea429735b4b3466492f53b7b9552361627e9a6a
Vicente <vishoxcl@gmail.com>
2025-10-30 12:48:25 -0300

    first commit

---
+++ src/main/java/com/levelup/catalogo_producto/CatalogoProductoApplication.java
@@ -0,0 +1,13 @@
+package com.levelup.catalogo_producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProductoApplication.class, args);
+	}
+
+}
